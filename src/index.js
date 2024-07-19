6cdaa9a73a158c821b98ef041f528de7f5a845c5
Emmanuel F. Borjal <emmanuel.borjal@awsys-i.com>
2024-07-19 21:24:15 +0800

    Printing JavaScript Variables in JSX

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 // 1) Import the React and ReactDOM libraries
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 // 2) Get a reference to the div with ID root
-const el = document.getElementById("root");
+const el = document.getElementById('root');
 
 // 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
 function App() {
-  return <h1>Hi there!</h1>;
+  let message = 'Bye there!';
+  if (Math.random() > 0.5) {
+    message = 'Hello there!';
+  }
+
+  return <h1>{message}</h1>;
 }
 
 // 5) Show the component on the screen
